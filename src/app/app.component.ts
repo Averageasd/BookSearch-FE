1d7eef5cbd1017ec6bda094ff0ffc67cdb6f5783
Nguyen Huynh <nhuynh9497@gmail.com>
2025-07-15 17:08:24 -0400

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+interface SortOrder {
+  value: string;
+  viewValue: string;
+};
+
+interface SortColumn {
+  value: string;
+  viewValue: string;
+};
+
+@Component({
+  selector: 'app-root',
+  providers:[],
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss' ,'../styles.scss']
+})
+
+export class AppComponent {
+  title = 'BookSearch-FE';
+  sortOrders: SortOrder[] = [
+    {value: 'ASC', viewValue: 'Ascending'},
+    {value: 'DESC', viewValue: 'Descending'},
+  ];
+
+  sortColumns: SortColumn[] = [
+    {value: 'created_at', viewValue: 'Book creation date'},
+    {value: 'copies', viewValue: 'Number of copies'},
+    {value: 'rating', viewValue: 'Book rating'},
+    {value: 'title', viewValue: 'Book title'}
+  ];
+}
